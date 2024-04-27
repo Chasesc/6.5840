7e3059739a818af107c0a4ccc4281ebbb67968d2
Chasesc <schelthoff.chase@gmail.com>
2024-04-27 18:04:40 -0400

    wip reducer

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,6 +1,7 @@
 package mr
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -9,33 +10,62 @@ import (
 	"sync"
 )
 
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+type mapState struct {
+	mapperStarted bool
+	taskNumber    int
+}
+
 type Coordinator struct {
 	// Your definitions here.
-	taskNumber          int
 	mu                  sync.RWMutex
 	numReduce           int
-	processedInputFiles map[string]bool
+	processedInputFiles map[string]*mapState
+	reducerStarted      []bool
 }
 
 // Your code here -- RPC handlers for the worker to call.
 func (c *Coordinator) GetTask(args *GetTaskArgs, reply *GetTaskReply) error {
 	c.mu.RLock()
 
-	for filename, processed := range c.processedInputFiles {
-		if !processed {
+	for filename, mrState := range c.processedInputFiles {
+		if !mrState.mapperStarted {
 			c.mu.RUnlock()
 			c.mu.Lock() // Acquire the write lock
 			defer c.mu.Unlock()
 
-			c.taskNumber += 1
-
 			// double check the condition after acquiring the write lock.
-			if !c.processedInputFiles[filename] {
+			if !c.processedInputFiles[filename].mapperStarted {
 				reply.InputFiles = []string{filename}
 				reply.NumReduce = c.numReduce
 				reply.IsMap = true
-				reply.TaskNumber = c.taskNumber
-				c.processedInputFiles[filename] = true
+				reply.TaskNumber = mrState.taskNumber
+				c.processedInputFiles[filename].mapperStarted = true
+			}
+			return nil
+		}
+	}
+
+	// All mappers have started. We can now start the reducers
+	for i := range c.reducerStarted {
+		if c.readyToStartReducer(i) {
+			c.mu.RUnlock()
+			c.mu.Lock() // Acquire the write lock
+			defer c.mu.Unlock()
+
+			// double check the condition after acquiring the write lock.
+			if !c.reducerStarted[i] {
+				reply.InputFiles = c.reducerFileNames(i)
+				reply.IsMap = false
+				reply.TaskNumber = i
+				c.reducerStarted[i] = true
 			}
 			return nil
 		}
@@ -43,6 +73,35 @@ func (c *Coordinator) GetTask(args *GetTaskArgs, reply *GetTaskReply) error {
 
 	c.mu.RUnlock()
 	return nil
+}
+
+func (c *Coordinator) reducerFileNames(reducerNumber int) []string {
+	numMappers := len(c.processedInputFiles)
+	filenames := make([]string, numMappers)
+	for i := 0; i < numMappers; i++ {
+		// reducers need all mappers to be done! We get all mapper inputs
+		// that have the same reduce task number
+		filenames[i] = fmt.Sprintf("mr-%d-%d.json", i, reducerNumber)
+	}
+
+	return filenames
+}
+
+func (c *Coordinator) readyToStartReducer(reducerNumber int) bool {
+	if c.reducerStarted[reducerNumber] {
+		return false // already started.
+	}
+
+	for _, reducerInputFilename := range c.reducerFileNames(reducerNumber) {
+		// our mapper should really create tempfiles and atomically rename so we don't
+		// return true when we are still writing to the file, but lets see if I decide to implement that.
+		if !fileExists(reducerInputFilename) {
+			fmt.Printf("cannot start reducer %v DNE\n", reducerInputFilename)
+			return false
+		}
+	}
+
+	return true
 }
 
 // an example RPC handler.
@@ -74,12 +133,6 @@ func (c *Coordinator) Done() bool {
 	defer c.mu.RUnlock()
 
 	// Your code here.
-	for _, processed := range c.processedInputFiles {
-		if !processed {
-			return false
-		}
-	}
-
 	return false // FIXME: return true when the reducers finish.
 }
 
@@ -87,13 +140,13 @@ func (c *Coordinator) Done() bool {
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	processedInputFiles := make(map[string]bool, len(files))
-
-	for _, filename := range files {
-		processedInputFiles[filename] = false
+	processedInputFiles := make(map[string]*mapState, len(files))
+	for i, filename := range files {
+		processedInputFiles[filename] = &mapState{taskNumber: i}
 	}
 
-	c := Coordinator{processedInputFiles: processedInputFiles, numReduce: nReduce}
+	reducerStarted := make([]bool, nReduce)
+	c := Coordinator{processedInputFiles: processedInputFiles, numReduce: nReduce, reducerStarted: reducerStarted}
 
 	c.server()
 	return &c
